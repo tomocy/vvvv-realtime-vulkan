9b94a403ee089521563a571d2a76e4a314f38398
tomocy <tomocy.dev@gmail.com>
2026-04-20 04:43:07 +0900

    Use vvvv::Scoped<VkInstance>

--- vk_instance.h
+++ vk_instance.h
@@ -34,15 +34,18 @@ public:
 namespace vvvv {
 struct CreateVkInstance {
 public:
-    [[nodiscard]] std::tuple<VkInstance, Error> invoke() const noexcept
+    [[nodiscard]] std::tuple<Scoped<VkInstance>, Error> invoke() const noexcept
     {
         VkInstance instance = VK_NULL_HANDLE;
         const auto result = vkCreateInstance(&info, allocator, &instance);
         if (result != VK_SUCCESS) {
-            return { VK_NULL_HANDLE, vvvv::Error(std::format("{}", result)) };
+            return { Scoped<VkInstance>(), vvvv::Error(std::format("{}", result)) };
         }
 
-        return { instance, vvvv::Error::none() };
+        return {
+            Scoped(instance, { .allocator = allocator }),
+            vvvv::Error::none()
+        };
     }
 
 public:
