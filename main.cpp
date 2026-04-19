28c20a3f5fc34910de9f42234f22745b75c5bcf9
tomocy <tomocy.dev@gmail.com>
2026-04-20 02:41:15 +0900

    Add run

--- main.cpp
+++ main.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
 
+#include "error.h"
+
+namespace {
+vvvv::Error run();
+} // namespace
+
 int main()
 {
-    std::cout << "Hello, World!\n";
+    const auto err = run();
+    if (err.has()) {
+        std::cerr << "Error: " << err.message() << "\n";
+        return 1;
+    }
     return 0;
 }
+
+namespace {
+vvvv::Error run()
+{
+    std::cout << "Completed\n";
+    return vvvv::Error::none();
+}
+} // namespace
