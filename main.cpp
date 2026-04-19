029cc3062159e1f052f3673e99d63eaebd9a5dd5
tomocy <tomocy.dev@gmail.com>
2026-04-20 03:58:02 +0900

    Create vkInstance

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <vulkan/vulkan_core.h>
 
 #include "error.h"
+#include "vk_instance.h"
 
 namespace {
 vvvv::Error run();
@@ -19,6 +21,27 @@ int main()
 namespace {
 vvvv::Error run()
 {
+    VkAllocationCallbacks* allocator = nullptr;
+
+    VkInstance instance = VK_NULL_HANDLE;
+    {
+        auto [v, err] = vvvv::CreateVkInstance()
+                            .with([&](auto& opts) {
+                                opts.appInfo.pApplicationName = "vvvv";
+                                opts.appInfo.apiVersion = VK_MAKE_API_VERSION(0, 1, 4, 0);
+                                opts.allocator = allocator;
+                            })
+                            .invoke();
+        if (err.has()) {
+            return vvvv::Error::wrap("creating vkInstance", err);
+        }
+
+        instance = v;
+        std::cout << "VkInstance: " << instance << "\n";
+    }
+
+    vkDestroyInstance(instance, allocator);
+
     std::cout << "Completed\n";
     return vvvv::Error::none();
 }
