4c42b06edbbe08b0380b80ed539df2ef9d0957db
tomocy <tomocy.dev@gmail.com>
2026-04-22 03:33:34 +0900

    Add vvvv::CreateVkDebugUtilsMessenger

--- vk_debug.h
+++ vk_debug.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <format>
+#include <utility>
 #include <vulkan/vulkan_core.h>
 
+#include "error.h"
+#include "result.h"
 #include "scoped.h"
+#include "vk_result.h" // IWYU pragma: keep
+#include "vk_struct.h"
 
 namespace vvvv {
 template <>
@@ -23,5 +29,63 @@ public:
 
         owner.vkDestroyDebugUtilsMessenger(owner.instance, messenger, owner.allocator);
     }
+};
+} // namespace vvvv
+
+namespace vvvv {
+struct CreateVkDebugUtilsMessenger {
+public:
+    explicit CreateVkDebugUtilsMessenger(VkInstance instance)
+        : instance(instance)
+    {
+    }
+
+public:
+    [[nodiscard]] Result::Either<Scoped<VkDebugUtilsMessengerEXT>, Error> invoke() const noexcept
+    {
+        if (vkCreateDebugUtilsMessenger == nullptr) {
+            return Result::Error(Error("vkCreateDebugUtilsMessenger not provided"));
+        }
+        if (vkDestroyDebugUtilsMessenger == nullptr) {
+            return Result::Error(Error("vkDestroyDebugUtilsMessenger not provided"));
+        }
+
+        VkDebugUtilsMessengerEXT messenger = VK_NULL_HANDLE;
+        const auto result = vkCreateDebugUtilsMessenger(instance, &info, allocator, &messenger);
+        if (result != VK_SUCCESS) {
+            return Result::Error(Error(std::format("{}", result)));
+        }
+
+        return Result::OK(
+            Scoped(
+                messenger,
+                {
+                    .instance = instance,
+                    .allocator = allocator,
+                    .vkDestroyDebugUtilsMessenger = vkDestroyDebugUtilsMessenger,
+                }
+            )
+        );
+    }
+
+public:
+    template <typename F>
+        requires std::invocable<F&, CreateVkDebugUtilsMessenger&>
+        && std::same_as<std::invoke_result_t<F&, CreateVkDebugUtilsMessenger&>, void>
+    CreateVkDebugUtilsMessenger& with(F&& f)
+    {
+        std::forward<F>(f)(*this);
+        return *this;
+    }
+
+public:
+    VkInstance instance = VK_NULL_HANDLE;
+
+    VkDebugUtilsMessengerCreateInfoEXT info = vkStructZero<VkDebugUtilsMessengerCreateInfoEXT>();
+
+    VkAllocationCallbacks* allocator = nullptr;
+
+    PFN_vkCreateDebugUtilsMessengerEXT vkCreateDebugUtilsMessenger = nullptr;
+    PFN_vkDestroyDebugUtilsMessengerEXT vkDestroyDebugUtilsMessenger = nullptr;
 };
 } // namespace vvvv
