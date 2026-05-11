2c2c6e90e73377b522fb469c1f2743caa4df98bf
tomocy <tomocy.dev@gmail.com>
2026-05-12 02:51:34 +0900

    Add vvvv::CreateVkGraphicsKernel

--- vk_graphics.h
+++ vk_graphics.h
@@ -1,5 +1,18 @@
 #pragma once
 
+#include "error.h"
+#include "result.h"
+
 namespace vvvv {
 struct VkGraphicsKernel { };
+} // namespace vvvv
+
+namespace vvvv {
+struct CreateVkGraphicsKernel {
+public:
+    Result::Either<VkGraphicsKernel, Error> operator()() const
+    {
+        return Result::OK(VkGraphicsKernel());
+    }
+};
 } // namespace vvvv
