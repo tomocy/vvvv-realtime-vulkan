6b3ea408a077230e60ea3fd88581a631e5f8527e
tomocy <tomocy.dev@gmail.com>
2026-05-12 01:11:54 +0900

    Add vvvv::CreateRenderer

--- renderer.h
+++ renderer.h
@@ -1,5 +1,18 @@
 #pragma once
 
+#include "error.h"
+#include "result.h"
+
 namespace vvvv {
 struct Renderer { };
+} // namespace vvvv
+
+namespace vvvv {
+struct CreateRenderer {
+public:
+    Result::Either<Renderer, Error> operator()() const noexcept
+    {
+        return Result::OK(Renderer {});
+    }
+};
 } // namespace vvvv
